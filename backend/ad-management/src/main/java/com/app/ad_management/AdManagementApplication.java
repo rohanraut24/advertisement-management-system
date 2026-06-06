3713e27d9de30d9d65f30e5ac7ca95f8d7b3d056
rohan raut <rohanraut2443@gmail.com>
2026-06-06 15:59:59 +0530

    Database setup

---
+++ backend/ad-management/src/main/java/com/app/ad_management/AdManagementApplication.java
@@ -0,0 +1,12 @@
+package com.app.ad_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdManagementApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AdManagementApplication.class, args);
+	}
+
+}
---
+++ backend/ad-management/src/test/java/com/app/ad_management/AdManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ad_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
